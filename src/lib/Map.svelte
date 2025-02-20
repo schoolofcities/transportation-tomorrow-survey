8ca75ae00b8617e2b5b62903ca4b8b0cf6de1643
mkbs-mkbs2000 <muhammadkhalisbinsamion@gmail.com>
2025-02-20 16:53:52 -0500

    The map shows each TTS Zone based on % of hh WITHOUT a vehicle. This is trimmed from the original tts-pple-hhlds-vehicles.csv file. For some reason, TTS Zones that have no data about hh w/o vehicles are shown in black instead of the grey as specified in the code. I have yet to fully work out the panel placement and therefore the check off and on button for the diff data that could be added in. This is really temporary at the moment, as I try to understand better on how this works.

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -6,10 +6,15 @@
 	import * as pmtiles from "pmtiles";
 	import layers from 'protomaps-themes-base';
 	import "../assets/global.css";
+	import ttszones from "../data/tts2022zones.geo.json";
+	import currentlines from "../data/CurrentTransitLines.geo.json";
+	import futurelines from "../data/FutureTransitLines.geo.json";
 
 	let map;
 	
 	let PMTILES_URL = 'https://api.protomaps.com/tiles/v4.json?key=7f48bb9c6a1f1e3b'
+
+	let colours = ["#f7ecc3", "#f2cd8d", "#eeb05b", "#e78052", "#e15449"];
 
 	onMount(async () => {
 
@@ -52,6 +57,96 @@
 
 		});
 
+		map.on('load', () => {
+
+			map.addSource('ttszones', {
+				type: 'geojson',
+				data: ttszones
+			});
+
+			map.addSource('currentlines', {
+				type: 'geojson',
+				data: currentlines
+			});
+
+			map.addSource('futurelines', {
+				type: 'geojson',
+				data: futurelines
+			});
+
+			map.addLayer(
+				{
+					'id': 'ttszones',
+					'type': 'fill',
+					'source': 'ttszones',
+					'paint': {
+						'fill-color': [
+							'step', ['get', 'Perc_No_Veh'],
+							colours[0], 0.2,
+							colours[1], 0.4,
+							colours[2], 0.6,
+							colours[3], 0.8,
+							colours[4], 1.01,
+							'#cbcbcb'
+						],
+						'fill-opacity': 0.5,
+						'fill-outline-color': '#000000'
+					}
+				}
+			);
+
+			map.addLayer(
+				{
+					'id':'currentlines',
+					'type':'line',
+					'source':'currentlines',
+					'paint': {
+						'line-color': '#006400',
+						'line-opacity': 0.95,
+						'line-width': 2 
+					}
+				}
+			)
+
+			map.addLayer(
+				{
+					'id':'futurelines',
+					'type':'line',
+					'source':'futurelines',
+					'paint': {
+						'line-color': '#6C3BAA',
+						'line-opacity': 0.95,
+						'line-width': 2,
+						'line-dasharray': [1.5, 1.5] 
+					}
+				}
+			);
+		});
+
+		const popup = new maplibregl.Popup({
+			closeButton: false,     // The idea is to make the popup close when the mouse leaves. So no close button shld be present on the popup
+			closeOnClick: false     // The idea is to make the popup close when the mouse leaves. So no clicking of mouse is required
+		});
+
+		// Initialising the mouseenter event to popup 
+		map.on('mouseenter', 'ttszones', (e) => {
+			map.getCanvas().style.cursor = 'pointer';                       // Changing the cursor to a pointer when the mouse hovers on the restaurant
+
+    		const coordinates = e.lngLat; 								// Getting the coordinates of the restaurant that the mouse is hovering on
+    		const description = e.features[0].properties.TTS2022 + '<br>' + e.features[0].properties.Perc_No_Veh;	// Setting description to be the percentage of hh with no vehicles
+
+   			popup
+			 .setLngLat(coordinates)                                     // Setting the popup to be located on the coordinates of the restaurant
+			 .setHTML(description)                                       // Setting the description in the popup
+			 .addTo(map);                                                // Adding the popup to the map
+		});
+
+		// Initialising the mouseleave event to remove the popup
+		map.on('mouseleave', 'ttszones', () => {
+			map.getCanvas().style.cursor = '';                              // Changing the cursor back to default when the mouse hovers away from the restaurant
+    		popup.remove();                                                 // Removing the popup from the map
+		});
+
 		const attributions = [
 			'<a href="https://openstreetmap.org" target= "_blank" style="color: black; text-decoration: underline;"> OpenStreetMap</a>'
 		];
@@ -70,21 +165,28 @@
 		});
 
 		map.addControl(scale, "bottom-right");
-
-		map.on("load", () => {
-
-			console.log("hello world")
-
-			// add in additional sources and layers in here when ready
-
-		});
 	
 	});
 
 </script>
 
+<div id='container'>
 
-<div id="map"></div>
+	<div id='panel'>
+
+		<div id='title'>
+
+			<h1>Transportation Tomorrow Survey</h1>
+
+			<h2>Results across the Greater Golden Horseshoe Region</h2>
+
+		</div>
+
+	</div>
+
+	<div id="map"></div>
+
+</div>
 
 
 <style>
